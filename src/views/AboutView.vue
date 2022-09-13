56400a8a456f199cfccfe3fc869c8f64c152f55d
chou <metagym960324@gmail.com>
2022-09-13 22:42:51 +0800

    addCard

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Card></Card>
   </div>
 </template>
 
 <script>
-
+import Card from '@/components/Bootstrap-Card.vue'
+export default {
+  Card
+}
 </script>
